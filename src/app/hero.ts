47f8708fa5ceb43c9a84b2eac07c302b9209ff54
vonshine740 <vonshine740@163.com>
2017-03-23 17:27:16 +0800

    Init

---
+++ src/app/hero.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by wangshuai1 on 2017/3/23.
+ */
+export class Hero {
+  constructor(
+    public id: number,
+    public name: string) { }
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by wangshuai1 on 2017/3/23.
+ */
+import { Hero } from './hero';
+
+// It is heroes data list.
+export const HEROES: Hero[] = [
+  new Hero(1, 'Windstorm'),
+  new Hero(13, 'Bombasto'),
+  new Hero(15, 'Magneta'),
+  new Hero(20, 'Tornado')
+];
